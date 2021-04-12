64913f7439cd7fdb1f074875d5cf4391f0da794b
Dylan J <dylan@tritan.gg>
2021-04-12 00:55:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// Imports
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path');
+const fs = require("fs");
+
+// Inits
+const app = express();
+
+// Standard
+app.disable("x-powered-by");
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+// Begin Web Server
+app.listen(process.env.port, () => {
+  console.log("Running");
+});
+
+loadRoutes();
+
+
+function loadRoutes() {
+  const routesPath = path.join(__dirname, "./routes");
+
+  fs.readdir(routesPath, (err, files) => {
+    if (err) {
+      throw err;
+    }
+
+    files.forEach((filename) => {
+      const route = require(path.join(routesPath, filename));
+
+      const routePath = filename === "ticket.js" ? "/" : `/${filename.slice(0, -3)}`;
+
+      try {
+        app.use(routePath, route);
+        console.log(`${routePath}`);
+      } catch (error) {
+        console.log(
+          `Error occured with the route "${filename}":\n\n${error} Ignoreing continuing`
+        );
+      }
+    });
+  });
+
+  return this;
+}
---
+++ routes/ticket.js
@@ -0,0 +1,110 @@
+//  Consts
+const { Router } = require("express");
+const nodemailer = require('nodemailer');
+
+// Inits
+var route = Router();
+const transporter = nodemailer.createTransport({
+  host: "mail.tritan.gg",
+  port: 465,
+  secure: true, // upgrade later with STARTTLS
+  auth: {
+    user: "jellyjim_csat@tritan.gg",
+    pass: "turrocke52"
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+// Home
+route.get("/", (req, res, next) => {
+  return res.redirect("https://jellyjim.com/404")
+});
+
+
+// Positive Review Collector
+route.get("/:ticket/positive", (req, res, next) => {
+
+  let data = {
+    ticket: req.params.ticket,
+    positive: true,
+    negative: false
+  }
+  return res.render("index", data);
+});
+
+// Positive Review Handler
+route.post("/:ticket/positive", (req, res, next) => {
+
+  const ticket = req.params.ticket;
+  const comment = req.body.comments;
+
+  var mailOptions = {
+    from: 'jellyjim_csat@tritan.gg',
+    to: 'hello@jellyjim.com',
+    subject: 'Ticket CSAT Feedback',
+    text: `The owner of ticket #${ticket} selected that they are happy with the resolution. \n \n Comments: \n ${comment}`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  let data = {
+    ticket: ticket,
+    comment: comment
+  }
+  res.render("submitted", data);
+});
+
+// Negative Review Collector
+route.get("/:ticket/negative", (req, res, next) => {
+
+  const ticket = req.params.ticket;
+  let data = {
+    ticket: ticket,
+    positive: false,
+    negative: true
+  }
+  return res.render("index", data);
+});
+
+// Negative Review Handler
+route.post("/:ticket/negative", (req, res, next) => {
+
+  const ticket = req.params.ticket;
+  const comment = req.body.comments;
+
+  var mailOptions = {
+    from: 'jellyjim_csat@tritan.gg',
+    to: 'hello@jellyjim.com',
+    subject: 'Ticket CSAT Feedback',
+    text: `The owner of ticket #${ticket} selected that they are unhappy with the resolution. \n \n Comments: \n ${comment}`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  let data = {
+    ticket: ticket,
+    comment: comment,
+  }
+  return res.render("submitted", data)
+});
+
+
+route.get("*", (req, res, next) => {
+  return res.redirect("/")
+});
+
+module.exports = route;
